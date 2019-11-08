4f798ad272ed6e198c0c2d3149eede3e30d359c7
Florian Spadaro <spadaro.florian@outlook.fr>
2019-11-08 15:52:45 +0100

    Ajout des nouvelles images de fond + début géolocalisation

--- app/src/main/java/com/example/dowaf/EditAliment.kt
+++ app/src/main/java/com/example/dowaf/EditAliment.kt
@@ -5,6 +5,9 @@ import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -12,6 +15,8 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_edit_aliment.*
 
 
@@ -20,14 +25,38 @@ class EditAliment : AppCompatActivity() {
     private val PERMISSION_CODE = 1000
     private val IMAGE_CAPTURE_CODE = 1001
     var image_uri: Uri? = null
+    private var locationManager : LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_aliment)
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { return; }
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?;
+
+
         val title = intent.getStringExtra("title")
 
         titleView.text = title
+    }
+    fun showlocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener);
+
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            var message =  location.longitude.toString() + ":" + location.latitude.toString()
+            //Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            // il faut retourner ou jour avec la variable message
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     fun onClickPictureBtn(view: View) {
