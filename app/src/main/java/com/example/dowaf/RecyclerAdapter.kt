15652c3598cc88b7ce6898a5a97d8d651615036a
Florian Spadaro <spadaro.florian@outlook.fr>
2019-11-14 17:01:52 +0100

    Suppression d'un aliment

--- app/src/main/java/com/example/dowaf/RecyclerAdapter.kt
+++ app/src/main/java/com/example/dowaf/RecyclerAdapter.kt
@@ -7,18 +7,22 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dowaf.model.Aliment
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
+
 
 class RecyclerAdapter(options: FirestoreRecyclerOptions<Aliment>) :
     FirestoreRecyclerAdapter<Aliment, RecyclerAdapter.ViewHolder>(options) {
     private val storage = FirebaseStorage.getInstance()
     private val auth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context)
@@ -58,6 +62,28 @@ class RecyclerAdapter(options: FirestoreRecyclerOptions<Aliment>) :
             itemDetail = itemView.findViewById(R.id.item_detail)
 
             //TODO Pouvoir Supprimer les aliments (LongClick / Swipe / Button)
+
+            itemView.setOnLongClickListener {
+                val aliment = getItem(adapterPosition)
+                aliment.id = snapshots.getSnapshot(adapterPosition).id
+
+                val builder = AlertDialog.Builder(it.context)
+                builder.setTitle("Suppression de l'aliment: " + aliment.name.toString())
+                builder.setMessage("Voulez-vous vraiment supprimer cet aliment?")
+                //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+                builder.setPositiveButton("Confirmer") { dialog, which ->
+                    db.collection("aliments").document(aliment.id.toString()).delete()
+                }
+
+                builder.setNegativeButton("Annuler") { dialog, which ->
+
+                }
+
+                builder.show()
+
+                true
+            }
 
             itemView.setOnClickListener {
                 val aliment = getItem(adapterPosition)
